fcc3de417b57b25213b96d94456b53ac72a5ec2b
buggavep <praneeth.buggaveeti@gmail.com>
2018-08-06 23:05:05 -0400

    added version and build jobs

--- scripts/version.sh
+++ scripts/version.sh
@@ -5,10 +5,10 @@ set -e -x
 ls -lrt
 
 PACKAGE_VERSION=$(cat package.json \ 
- | grep version \
- | head -1 \
- | awk -F: '{ print $2 }' \
- | sed 's/[",]//g' \
- | tr -d '[[:space:]]')
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
 
- echo $PACKAGE_VERSION
+echo $PACKAGE_VERSION
