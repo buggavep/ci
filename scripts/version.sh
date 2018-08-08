5a3396622d89441736924bc4fe1d89d6a44fd71d
buggavep <praneeth.buggaveeti@gmail.com>
2018-08-08 00:39:39 -0400

    added version and build jobs

--- scripts/version.sh
+++ scripts/version.sh
@@ -15,11 +15,9 @@ PACKAGE_VERSION=$(cat package.json \
 echo "current package version:${PACKAGE_VERSION}"
 #cd ..
 ls -la
-touch version
-chmod 777 version
-ls -la
+
 #echo $(date) > version
-echo "$PACKAGE_VERSION" > ../updated-gist/version
+echo "$PACKAGE_VERSION" > ../updated-gist/version-gist
 
 ls -lrt
 
