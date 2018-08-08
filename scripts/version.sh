ea4b405e38bb5711facc5405152d58461a6584ae
buggavep <praneeth.buggaveeti@gmail.com>
2018-08-07 20:30:13 -0400

    added version and build jobs

--- scripts/version.sh
+++ scripts/version.sh
@@ -4,26 +4,25 @@ set -e
 
 ls -lrt
 
-# rm ../updated-gist/$GIST_NAME
-
-git clone version-gist updated-gist 
-
 PACKAGE_VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g' \
-  | tr -d '[[:space:]]')
+| grep version \
+| head -1 \
+| awk -F: '{ print $2 }' \
+| sed 's/[",]//g' \
+| tr -d '[[:space:]]')
 
-echo $PACKAGE_VERSION
-
+echo $PACKAGE_VERSION > ../updated-gist/version
+echo "current package version:${PACKAGE_VERSION}"
+#cd ..
+ls -la
+touch version
+chmod 777 version
+ls -la
+#echo $(date) > version
 echo "$PACKAGE_VERSION" > version
-
-mv version updated-gist
-
-cd ../updated-gist
-
 git config --global user.email "nobody@concourse.ci"
 git config --global user.name "Concourse"
 git add .
 git commit -m "Bumped Release"
+
+
