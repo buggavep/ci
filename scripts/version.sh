d83ce788123ee3ccbc828864143e9cc51debd9e8
buggavep <praneeth.buggaveeti@gmail.com>
2018-08-07 20:41:48 -0400

    added version and build jobs

--- scripts/version.sh
+++ scripts/version.sh
@@ -4,16 +4,18 @@ set -e
 
 ls -lrt
 
+git clone angular-app some-files1
 git clone version-gist updated-gist
-
+mv some-files1/package.json updated-gist
+cd updated-gist
+ls -la
+#npm pack
 PACKAGE_VERSION=$(cat package.json \
 | grep version \
 | head -1 \
 | awk -F: '{ print $2 }' \
 | sed 's/[",]//g' \
 | tr -d '[[:space:]]')
-
-echo $PACKAGE_VERSION > ../updated-gist/version
 echo "current package version:${PACKAGE_VERSION}"
 #cd ..
 ls -la
@@ -22,9 +24,9 @@ chmod 777 version
 ls -la
 #echo $(date) > version
 echo "$PACKAGE_VERSION" > version
-git config --global user.email "nobody@concourse.ci"
-git config --global user.name "Concourse"
-git add .
-git commit -m "Bumped Release"
-
+git config --global user.email "nobody@nobody.com"
+git config --global user.name "nobody"
+#git pull
+git add version
+git commit -m "Bumped release"
 
