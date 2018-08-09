a48f21e54be86fc1343daa3c7fe0665e54ac9663
buggavep <praneeth.buggaveeti@gmail.com>
2018-08-08 22:59:25 -0400

    added artifactory job

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,3 +8,6 @@ npm install
 
 ls -lrt
 
+tar -czvf ../dist/cibuild.tar.gz --exclude='./node_modules' --exclude='./.git' --exclude='./.tmp' .
+
+curl -u ${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} -T dist/cibuild* "http://0.0.0.0:8081/artifactory/example-repo-local/version/"
