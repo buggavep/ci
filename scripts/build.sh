8116c19fc1516e8255a4a124a556eb873a7fd1a7
buggavep <praneeth.buggaveeti@gmail.com>
2018-08-10 00:20:03 -0400

    added artifactory job

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,4 +10,6 @@ ls -lrt
 
 tar -czvf ../dist/cibuild.tar.gz --exclude='./node_modules' --exclude='./.git' --exclude='./.tmp' .
 
+ls -lrt ../dist
+
 # curl -u ${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} --upload-file ../dist/* http://10.0.0.101:9001/nexus/content/repositories/version/
