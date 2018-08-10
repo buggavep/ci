449e0e7f068a5b80c47b11d59e2e6e91d29bdb44
buggavep <praneeth.buggaveeti@gmail.com>
2018-08-09 22:17:27 -0400

    added artifactory job

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,4 +10,4 @@ ls -lrt
 
 tar -czvf ../dist/cibuild.tar.gz --exclude='./node_modules' --exclude='./.git' --exclude='./.tmp' .
 
-curl -v -u ${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} --upload-file angular.json http://10.0.0.101:9001/repository/maven-central/org/foo/1.0/foo-1.0.pom
+curl -v -u ${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} --upload-file angular.json http://10.0.0.101:9001/repository/version
