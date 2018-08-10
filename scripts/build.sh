e8be67e29c4c52e9b04a2d6a54b4b6e58a6ac594
buggavep <praneeth.buggaveeti@gmail.com>
2018-08-09 21:13:39 -0400

    added artifactory job

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,4 +10,4 @@ ls -lrt
 
 tar -czvf ../dist/cibuild.tar.gz --exclude='./node_modules' --exclude='./.git' --exclude='./.tmp' .
 
-curl -u${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} -T ../dist/cibuild.tar.gz "http://0.0.0.0:8081/artifactory/example-repo-local/version/"
+curl -u${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} -T ../dist/cibuild.tar.gz "http://10.0.0.101:8081/artifactory/example-repo-local/version/"
