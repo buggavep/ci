fc7ea24cad2e4e7298d9961bae83c1aa52c7139b
Praneeth Buggaveeti <praneeth.buggaveeti@gmail.com>
2019-02-04 16:29:37 -0500

    added nexus job and added nexus configuration

--- scripts/build.sh
+++ scripts/build.sh
@@ -27,4 +27,4 @@ cd ../dist
 
 ls -lrt
 
-curl --upload-file cibuild.tar.gz -u admin:admin123 -v http://localhost:8081/repository/demo-app/${cur_version}
+curl --upload-file cibuild.tar.gz -u admin:admin123 -v http://172.17.17.104:8081/repository/demo-app/${cur_version}
