b0670206bbb7745ee34a4d62066c2d2c0c8beb7b
Praneeth Buggaveeti <praneeth.buggaveeti@gmail.com>
2019-02-04 23:57:34 -0500

    pulling artifacts and deploy to pcf demo miracle

--- scripts/cfdeploy.sh
+++ scripts/cfdeploy.sh
@@ -4,10 +4,18 @@ set -e -x
 
 cur_version=$(cat ../version-gist/version-gist)
 
+BLUE=${APP_NAME}
+GREEN=${APP_NAME}-bg
 # curl -O ${NEXUS_HOST}/repository/demo-files/${cur_version}/cibuild.tar.gz
 
 curl -K -L "${NEXUS_HOST}/repository/demo-files/${cur_version}/cibuild.tar.gz" | tar -xvzf -
 
 cf login -a ${CF_API_ENDPOINT} -u ${CF_USERNAME} -p ${CF_PASSWORD} -o ${CF_ORG} -s ${CF_SPACE} >/dev/null 2>&1
+ 
+cf push ${GREEN)
 
-cf push ${APP_NAME}
+cf map-route ${BLUE}
+
+cf unmap-route ${GREEN}
+
+cf restage
