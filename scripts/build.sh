878ea89d779b7ebe54f8b210b3d1ce81da4701ca
Praneeth Buggaveeti <praneeth.buggaveeti@gmail.com>
2019-02-04 22:19:59 -0500

    edited

--- scripts/build.sh
+++ scripts/build.sh
@@ -28,4 +28,4 @@ cd ../dist
 chmod 744 cibuild.tar.gz
 ls -lart
 
-curl -v -sS --upload-file cibuild.tar.gz -u ${NEXUS_USERNAME}:${NEXUS_PASSWORD} ${NEXUS_HOST}/repository/demo-files/${cur_version}/cibuild.tar.gz > /dev/null 2>&1
+curl --silent  -v --upload-file cibuild.tar.gz -u ${NEXUS_USERNAME}:${NEXUS_PASSWORD} ${NEXUS_HOST}/repository/demo-files/${cur_version}/cibuild.tar.gz > /dev/null 2>&1
