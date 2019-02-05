e18b0421240f5047acd7375950f9423912d0e666
Praneeth Buggaveeti <praneeth.buggaveeti@gmail.com>
2019-02-04 22:48:11 -0500

    pulling artifacts configuraiton in deploy job

--- scripts/cfdeploy.sh
+++ scripts/cfdeploy.sh
@@ -4,7 +4,7 @@ set -e -x
 
 ls -lrt
 
-curl -O -u ${NEXUS_USERNAME}:${NEXUS_PASSWORD} ${NEXUS_HOST}/repository/demo-files/${cur_version}/*
+curl -O ${NEXUS_HOST}/repository/demo-files/${cur_version}/*
 
 ls -lrt
 
