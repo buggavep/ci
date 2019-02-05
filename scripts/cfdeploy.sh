649461201e4c8885e375b80042073ec5d248dba6
Praneeth Buggaveeti <praneeth.buggaveeti@gmail.com>
2019-02-04 23:30:33 -0500

    pulling artifacts and deploy to pcf demo miracle

--- scripts/cfdeploy.sh
+++ scripts/cfdeploy.sh
@@ -8,6 +8,6 @@ cur_version=$(cat ../version-gist/version-gist)
 
 curl -K -L "${NEXUS_HOST}/repository/demo-files/${cur_version}/cibuild.tar.gz" | tar -xvzf -
 
-cf login -a ${CF_API_ENDPOINT} -u ${CF_USERNAME} -p {CF_PASSWORD} -o ${ORG} -s {SPACE}
+cf login -a ${CF_API_ENDPOINT} -u ${CF_USERNAME} -p ${CF_PASSWORD} -o ${ORG} -s ${SPACE}
 
 cf push
